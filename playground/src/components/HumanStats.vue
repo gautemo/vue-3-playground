d2f94fe5c9667179b0cc5530579a81f2228b86b3
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-04-30 18:03:19 +0200

    figured out how to emit to custom multiple v-models

--- playground/src/components/HumanStats.vue
+++ playground/src/components/HumanStats.vue
@@ -2,11 +2,11 @@
     <div>
         <label>
             Age:
-            <input type="number" v-model="age" v-on:input="$attrs['onUpdate:age']($event.target.value)" step="3">
+            <input type="number" v-model="age" v-on:input="$emit('update:age', $event.target.value)" step="3">
         </label>
         <label>
             Height:
-            <input type="number" v-model="height" v-on:input="$attrs['onUpdate:height']($event.target.value)" step="3">
+            <input type="number" v-model="height" v-on:input="$emit('update:height', $event.target.value)" step="3">
         </label>
     </div>
 
